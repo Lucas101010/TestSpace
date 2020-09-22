29e5c224a7c6698d29fe4b00228ef739ecb6b65c
Lucas101010 <71681249+Lucas101010@users.noreply.github.com>
2020-09-22 12:51:46 +0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,12 +3,14 @@ overlay.setAttribute('id', 'overlay');
 var oImg = document.createElement("img");
 oImg.setAttribute('src', 'http://www.libpng.org/pub/png/img_png/IceAlpha-0.5.png');
 oImg.setAttribute('alt', 'na');
-oImg.setAttribute('height', '0%');
-oImg.setAttribute('width', '0%]');
+oImg.setAttribute('height', '100%');
+oImg.setAttribute('width', '100%');
 overlay.appendChild(oImg);
 
 document.body.appendChild(overlay);
 
+
+/*
 if(window.innerHeight > window.innerWidth){
     //alert("This website is best viewed in Landscape");
     oImg.setAttribute('height', '100%');
@@ -31,3 +33,4 @@ window
             
         }
     });
+*/
