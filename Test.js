55ee3c1be8aba94767b7886945b4034eb157ab58
Lucas101010 <71681249+Lucas101010@users.noreply.github.com>
2020-09-22 10:18:40 +0800

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,34 @@
+var overlay = document.createElement("div");
+overlay.setAttribute('id', 'overlay');
+
+var oImg = document.createElement("img");
+oImg.setAttribute('src', 'https://r1.ilikewallpaper.net/iphone-wallpapers/download/78275/Sea-foam-iphone-wallpaper-ilikewallpaper_com.jpg');
+oImg.setAttribute('alt', 'na');
+oImg.setAttribute('height', '0%');
+oImg.setAttribute('width', '0%]');
+overlay.appendChild(oImg);
+
+document.body.appendChild(overlay);
+
+if(window.innerHeight > window.innerWidth){
+    //alert("This website is best viewed in Landscape");
+    oImg.setAttribute('height', '100%');
+    oImg.setAttribute('width', '100%');
+}
+
+window
+    .matchMedia('(orientation: portrait)')
+    .addListener(function (m) {
+        if (m.matches) {
+            // portrait
+            //alert("This website is best viewed in Landscape");
+            oImg.setAttribute('height', '100%');
+            oImg.setAttribute('width', '100%');
+            
+        } else {
+            oImg.setAttribute('height', '0%');
+            oImg.setAttribute('width', '0%');
+            // landscape
+            
+        }
+    });
