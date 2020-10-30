c7ec6c14e9cdb2176fe9a1231075fe61074a9b39
Lucas101010 <71681249+Lucas101010@users.noreply.github.com>
2020-10-30 18:02:14 +0800

    Update test_radio.js

--- test_radio.js
+++ test_radio.js
@@ -1,4 +1,4 @@
-//version 12
+//version 15
 var vintage_radio = document.getElementById('vintage_radio');
 
 if(Hls.isSupported()) {
@@ -6,9 +6,26 @@ if(Hls.isSupported()) {
   hls.loadSource('https://hls.rastream.com/vintageradiosg-vintage.web.hls/playlist.m3u8?authtoken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvaWQiOiJsYXlsaW8iLCJpYXQiOjE2MDQwMjE3OTgsImV4cCI6MTYwNDEwODE5OH0.wARGspn6fo1rvOhjLH07qE3zoJAMhP0IISDCYiYHSUE');
   hls.attachMedia(vintage_radio);
   hls.on(Hls.Events.MANIFEST_PARSED,function() {
-    vintage_radio.play();
-    document.addEventListener('radio_volumeChange', function(e) {
-      vintage_radio.volume = e.detail;
+    
+    var started = false;
+    
+    document.addEventListener('onmousedown', function(e) {
+      readyforplaying();
+      started = true;
     });
+    
+    document.addEventListener('onkeydown', function(e) {
+      readyforplaying();
+      started = true;
+    });
+    
+    function readyforplaying(){
+      if(started)return;
+      vintage_radio.play();
+      vintage_radio.volume = 1.0;
+      document.addEventListener('radio_volumeChange', function(e) {
+      vintage_radio.volume = e.detail;
+      });
+    }
   });
 }
